930bb6fb32bcd75cce276ea51e3d61fb95186d09
Frederik Ejstrup Landgraf <frl001@edu.zealand.dk>
2025-02-24 11:56:59 +0100

    Program opsætning

--- PasswordCrackerMaster/Program.cs
+++ PasswordCrackerMaster/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PasswordCrackerMaster;
+using System.Net;
+
 Console.WriteLine("Hello, World!");
+
+namespace PasswordCrackerMaster
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Master master = new Master();
+
+			master.Listen(IPAddress.Loopback, 6789);
+		}
+	}
+}
+
+namespace PasswordCrackerSlave
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Slave slave = new Slave();
+
+			slave.connect("localhost", 6789);
+
+
+		}
+	}
+
+}
